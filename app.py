96ee3b438a13573458cb374f2b7ba5b751e01cc8
lrfussell <94795007+lrfussell@users.noreply.github.com>
2021-12-03 10:13:20 -0600

    Add files via upload

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ def main():
                                                      'PATCO':PATCO,
                                                      'STEMOCC':STEMOCC,
                                                      'LOS':LOS},
-                                     result = round(prediction, 0),
+                                     result = int(prediction),
                                      )
 
 if __name__ == '__main__':
